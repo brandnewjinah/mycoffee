76c0d962540d391e59deb7a447035badcfcff7a6
Jinah Lee <brandnewjinah@gmail.com>
2022-01-01 09:15:27 -0800

    8. Brew - Add Roaster autocomplete search for roaster names

--- frontend/src/pages/brew/add/AddBean.tsx
+++ frontend/src/pages/brew/add/AddBean.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
 import styled from "styled-components";
 
 //comp
@@ -7,9 +8,44 @@ import Header from "../../../components/Header";
 import { Input } from "../../../components/Input";
 import { Section } from "../../../components/container/Section";
 
+export interface StateProps {
+  id: number;
+  name: string;
+}
+
 const AddBean = () => {
   const history = useHistory();
-  const handleChange = () => {};
+  const [roasters, setRoasters] = useState<StateProps[]>([]);
+  const [suggestions, setSuggestions] = useState<StateProps[]>([]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await axios.get("/data/coffeeData.json");
+
+      setRoasters(response.data.roasters);
+    };
+
+    loadData();
+  }, []);
+
+  const handleChange = (text: string) => {
+    let matches: { id: number; name: string }[] = [];
+    if (text.length > 0) {
+      matches = roasters.filter((roaster) => {
+        const regex = new RegExp(`${text}`, "gi");
+        return roaster.name.match(regex);
+      });
+    }
+    setSuggestions(matches);
+    setText(text);
+  };
+
+  const onSuggest = (text: string) => {
+    setText(text);
+    setSuggestions([]);
+  };
+
   const handleClick = () => {
     history.push("/brew/note");
   };
@@ -18,8 +54,23 @@ const AddBean = () => {
     <Container>
       <Header title="What are you brewing today?" />
       <Section>
-        <Input label="Roaster" name="roaster" onChange={handleChange} />
-        <Input label="Name" name="name" onChange={handleChange} />
+        <input
+          value={text}
+          onChange={(e) => handleChange(e.target.value)}
+          onBlur={() => {
+            setTimeout(() => {
+              setSuggestions([]);
+            }, 100);
+          }}
+        />
+        {suggestions &&
+          suggestions.map((suggestion, i) => (
+            <Sug key={i} onClick={() => onSuggest(suggestion.name)}>
+              {suggestion.name}
+            </Sug>
+          ))}
+        {/* <Input label="Roaster" name="roaster" onChange={handleChange} />
+        <Input label="Name" name="name" onChange={handleChange} /> */}
         <span>roast level: light, medium, dark</span>
       </Section>
       <button onClick={handleClick}>next</button>
@@ -28,5 +79,16 @@ const AddBean = () => {
 };
 
 const Container = styled.div``;
+
+const Sug = styled.div`
+  cursor: pointer;
+  border-right: 1px solid black;
+  border-left: 1px solid black;
+  border-bottom: 1px solid black;
+
+  &:hover {
+    background-color: gray;
+  }
+`;
 
 export default AddBean;
